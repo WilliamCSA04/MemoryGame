7e99d761a91abbf89ba8bc997af103d9c36c7c8b
Williamcsa04 <williamcaetano14@gmail.com>
2018-11-22 07:13:50 -0200

    Created table

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,39 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Board {
     
     private static Board board = null;
+    private String table[][];
+
+    private Board(){
+        this.table = initializeTable();
+    }
     
     public static Board getInstance(){
         if(board == null){
             board = new Board();
         }
         return board;
+    }
+    
+    private String[][] initializeTable(){
+        Queue<String> options = getOptions();
+        String table[][]  = new String[6][3];
+        for(int i=0; i<table.length; i++){
+            for(int j=0; j<table.length; j++){
+                table[i][j] = options.remove();
+            }
+        }
+        return table;
+    }
+    
+    private Queue<String> getOptions(){
+        String allOptions[] = new String[] {"A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I"};
+        Arrays.sort(allOptions);
+        Queue<String> options = new LinkedList<>(Arrays.asList(allOptions));
+        return options;
     }
     
 }
