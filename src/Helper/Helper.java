9bcb914c91d5e614d6bfb7a681dc49844e3e5962
William Caetano <williamcaetano14@gmail.com>
2018-11-22 15:50:18 -0200

    Removed useless stuffs

--- src/Helper/Helper.java
+++ src/Helper/Helper.java
@@ -12,8 +12,6 @@ public class Helper {
         final String[][] result = new String[original.length][];
         for (int i = 0; i < original.length; i++) {
             result[i] = Arrays.copyOf(original[i], original[i].length);
-            // For Java versions prior to Java 6 use the next:
-            // System.arraycopy(original[i], 0, result[i], 0, original[i].length);
         }
         return result;
      }
