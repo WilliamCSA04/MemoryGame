a92a99ddfad3690858fa70f73cbd048161df5750
Williamcsa04 <williamcaetano14@gmail.com>
2018-11-22 07:59:00 -0200

    Added play method

--- src/Game.java
+++ src/Game.java
@@ -27,8 +27,38 @@ public class Game {
         return game;
     }
     
-    private boolean didLoss(){
+    private boolean didLose(){
         return MISS_LIMIT <= player.getMissTimes();
+    }
+    
+    public int play(String firstCoord[], String secondCoord[]){
+        int gameStatus = 0; //If has no winner or losser
+        if(firstCoord.length != 2 || secondCoord.length != 2){
+            return Integer.MIN_VALUE; //invalid play
+        }
+        if(firstCoord[0] == secondCoord[0] && firstCoord[1] == secondCoord[1]){
+            return Integer.MIN_VALUE; //invalid play
+        }
+        if(didLose()){
+            gameStatus = -1;
+        }else{
+            if(didWin()){
+                gameStatus = 1;
+            }
+        }
+        return gameStatus;
+    }
+    
+    private boolean didWin(){
+        String table[][] = board.getHiddenTable();
+        for(int i=0; i<table.length; i++){
+            for(int j=0; j<table[i].length; j++){
+                if(table[i][j].equals("?")){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
     
     private boolean finishGame(boolean isWinner){
